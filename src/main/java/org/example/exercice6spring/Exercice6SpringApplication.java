1a7fd3ae5e97e7924111076e55c35d79b9dbb82a
Lucie Roge <roge.lucie.dev@gmail.com>
2024-07-26 11:27:04 +0200

    update

--- src/main/java/org/example/exercice6spring/Exercice6SpringApplication.java
+++ src/main/java/org/example/exercice6spring/Exercice6SpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Exercice6SpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Exercice6SpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Exercice6SpringApplication.class, args);
+    }
 
 }
--- src/main/java/org/example/exercice6spring/service/TodoService.java
+++ src/main/java/org/example/exercice6spring/service/TodoService.java
@@ -1,2 +1,36 @@
-package org.example.exercice6spring.service;public class TodoService {
+package org.example.exercice6spring.service;
+
+import org.example.exercice6spring.dao.ITodoRepository;
+import org.example.exercice6spring.entity.Todo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private final ITodoRepository todoRepository;
+
+    public TodoService(ITodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public Todo save(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public void delete(Long id) {
+        todoRepository.deleteById(id);
+    }
+
+    public Todo findById(Long id) {
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    public List<Todo> findAll() {
+        return (List<Todo>) todoRepository.findAll();
+    }
+
+    public List<Todo> findByIsValidated(boolean status) {
+        return todoRepository.findByIsValidated(status);
+    }
 }
